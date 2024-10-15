8721ae889c0251c3a22e2b3c727c5c942ca4aace
user.email <skyeguy2003@gmail.com>
2024-10-15 16:01:07 -0700

    Test commit

--- src/Deque/LinkedDeque.java
+++ src/Deque/LinkedDeque.java
@@ -1,2 +1,120 @@
-public class LinkedDeque {
+import org.w3c.dom.Node;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedDeque<T> implements DequeInterface<T> {
+    private DLNode<T> front;
+    private DLNode<T> back;
+    private int size;
+
+    @Override
+    public void addToFront(T newEntry) {
+
+    }
+
+    @Override
+    public void addToBack(T newEntry) {
+
+    }
+
+    @Override
+    public T removeFront() {
+        return null;
+    }
+
+    @Override
+    public T removeBack() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public T getFront() {
+        return null;
+    }
+
+    @Override
+    public T getBack() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Iterator<T> getIterator() {
+        return null;
+    }
+
+    //DLNode class
+    private static class DLNode<T> {
+        private T data;
+        private DLNode<T> next;
+        private DLNode<T> previous;
+
+        public DLNode(T data) {
+            this.data = data;
+            this.next = null;
+            this.previous = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public DLNode<T> getNextNode() {
+            return next;
+        }
+
+        public void setNextNode(DLNode<T> next) {
+            this.next = next;
+        }
+
+        public DLNode<T> getPreviousNode() {
+            return previous;
+        }
+
+        public void setPreviousNode(DLNode<T> previous) {
+            this.previous = previous;
+        }
+    }
+
+    private class IteratorForLinkedList implements Iterator<T> {
+        private DLNode<T> current;
+
+        public IteratorForLinkedList() {
+            current = front;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T data = current.getData();
+            current = current.getNextNode();
+            return data;
+        }
+    }
 }
---
+++ src/DequeTest/LinkedDequeTest.java
@@ -0,0 +1,42 @@
+class LinkedDequeTest {
+
+    @org.junit.jupiter.api.Test
+    void addToFront() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addToBack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFront() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeBack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFront() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void iterator() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getIterator() {
+    }
+}
